86e53cb60ab622695ec81825398d57de636e04f3
solarapptech <149142801+solarapptech@users.noreply.github.com>
2023-10-27 02:55:40 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require ('express')
+const app = express ()
+const port = 8000
+
+
+const tasabcv = 35.02
+const tasamonitor = 37.24
+
+let date = new Date();
+let options = {timeZone: 'America/Caracas'};
+let caracas = date.toLocaleString('es-VE', options);
+
+
+app.get ('/info1', (req, res) =>{
+   res.setHeader('Content-Type', 'text/event-stream')
+   res.setHeader('Access-Control-Allow-Origin', '*')
+
+   const intervalId = setInterval(() =>{
+    const sendData = `data: ${JSON.stringify(tasabcv) +' Bs.'}\n\n`;
+    res.write(sendData);
+   },800)
+
+
+
+app.get ('/info2', (req, res) =>{
+      res.setHeader('Content-Type', 'text/event-stream')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+
+      const intervalId = setInterval(() =>{
+      const sendData2 = `data: ${JSON.stringify(tasamonitor) +' Bs.'}\n\n`;
+      res.write(sendData2);
+    },800)
+
+
+    app.get ('/info3', (req, res) =>{
+      res.setHeader('Content-Type', 'text/event-stream')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+
+      const sendData3 = `data: ${JSON.stringify(caracas)}\n\n`;
+      res.write(sendData3);
+      
+
+
+      res.on('close', () =>{
+        console.log('Client Closed Connection')
+        clearInterval(intervalId)
+      res.end()
+    })
+  })
+  })
+})
+
+
+app.listen(port,() => {
+    console.log (`Server Running on port ${port}`)
+})
