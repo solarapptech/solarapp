f99044d58fb9ec6c3679bd77e99ac287c938e363
solarapptech <149142801+solarapptech@users.noreply.github.com>
2023-10-27 20:10:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express ()
 const port = 8000
 
 
-const tasabcv = 35.30
-const tasamonitor = 37.24
+const tasabcv = 35.50
+const tasamonitor = 37.14
 
 let date = new Date();
 let options = {timeZone: 'America/Caracas'};
@@ -15,10 +15,10 @@ app.get ('/info1', (req, res) =>{
    res.setHeader('Content-Type', 'text/event-stream')
    res.setHeader('Access-Control-Allow-Origin', '*')
 
-   const intervalId = setInterval(() =>{
+    setInterval(() =>{
     const sendData = `data: ${JSON.stringify(tasabcv) +' Bs.'}\n\n`;
     res.write(sendData);
-   },800)
+   },800);
 
 
 
@@ -26,24 +26,25 @@ app.get ('/info2', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
 
-      const intervalId = setInterval(() =>{
+      setInterval(() =>{
       const sendData2 = `data: ${JSON.stringify(tasamonitor) +' Bs.'}\n\n`;
       res.write(sendData2);
-    },800)
+    },800);
 
 
     app.get ('/info3', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
-
+       
+      setInterval(() =>{
       const sendData3 = `data: ${JSON.stringify(caracas)}\n\n`;
       res.write(sendData3);
-      
+      },800);
 
 
       res.on('close', () =>{
         console.log('Client Closed Connection')
-        clearInterval(intervalId)
+        clearInterval()
       res.end()
     })
   })
