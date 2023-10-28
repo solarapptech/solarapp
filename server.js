d9d0bd803cc13370748fe911b219cc441c38155a
solarapptech <149142801+solarapptech@users.noreply.github.com>
2023-10-27 20:28:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ let options = {timeZone: 'America/Caracas'};
 let caracas = date.toLocaleString('es-VE', options);
 
 
-
 app.get ('/info1', (req, res) =>{
    res.setHeader('Content-Type', 'text/event-stream')
    res.setHeader('Access-Control-Allow-Origin', '*')
 
+   const intervalId = setInterval(() =>{
     const sendData = `data: ${JSON.stringify(tasabcv) +' Bs.'}\n\n`;
     res.write(sendData);
-
+   },800)
 
 
 
@@ -26,12 +26,13 @@ app.get ('/info2', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
 
+      const intervalId = setInterval(() =>{
       const sendData2 = `data: ${JSON.stringify(tasamonitor) +' Bs.'}\n\n`;
       res.write(sendData2);
+    },800)
 
 
-
-app.get ('/info3', (req, res) =>{
+    app.get ('/info3', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
 
@@ -40,18 +41,13 @@ app.get ('/info3', (req, res) =>{
       
 
 
-res.on('close', () =>{
-      console.log('Client Closed Connection')
-      clearInterval(intervalId)
+      res.on('close', () =>{
+        console.log('Client Closed Connection')
+        clearInterval(intervalId)
       res.end()
     })
-   })
   })
-})
-
-
-app.listen(port,() => {
-    console.log (`Server Running on port ${port}`)
+  })
 })
 
 
