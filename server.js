14ea3da26b8b3d26efff2080f1d4f11054cdff6d
solarapptech <149142801+solarapptech@users.noreply.github.com>
2024-01-17 13:26:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get ('/info2', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
 
-      const sendData3 = `data: ${JSON.stringify('Mar 17   -   1:30 PM')}\n\n`;
+      const sendData3 = `data: ${JSON.stringify('Jue 17   -   1:30 PM')}\n\n`;
       res.write(sendData3);
       
 
