3aa00a369540b907e57ea9f394f9749c46528fe2
solarapptech <149142801+solarapptech@users.noreply.github.com>
2024-02-01 11:33:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ const percent = tasaeuro / tasabcv
 
 
 app.get ('/info1', (req, res) =>{
-   res.setHeader('Content-Type', 'text/event-stream')
-   res.setHeader('Access-Control-Allow-Origin: https://render.com/')
+   res.setHeader('Content-Type', 'text/event-stream');
+   res.setHeader('Access-Control-Allow-Origin: https://render.com');
 
    const intervalId = setInterval(() =>{
     const sendData = `data: ${JSON.stringify(tasabcv) +' Bs.'}\n\n`;
