ef304a7c26be6cae6e1db761f5691ab3db76ea2e
solarapptech <149142801+solarapptech@users.noreply.github.com>
2024-02-01 13:47:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
 
 app.get ('/info1', (req, res) =>{
    res.setHeader('Content-Type', 'text/event-stream')
-   res.setHeader('Access-Control-Allow-Origin', 'https://solartech.onrender.com/info1')
+   res.setHeader('Access-Control-Allow-Origin', 'https://solartech.onrender.com')
 
    const intervalId = setInterval(() =>{
     const sendData = `data: ${JSON.stringify(tasabcv) +' Bs.'}\n\n`;
