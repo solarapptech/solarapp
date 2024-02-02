ff1dfba98d6b9c9546f88d9d87133f9a16838e80
solarapptech <149142801+solarapptech@users.noreply.github.com>
2024-02-02 01:47:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -80,55 +80,57 @@ app.get ('/info2', (req, res, next) =>{
 
 
 
-    app.get ('/info3', (req, res) =>{
+    app.get ('/info3', (req, res, next) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
 
       const intervalId = setInterval(() =>{
         const sendData3 = `data: ${JSON.stringify(fechas)}\n\n`
         res.write(sendData3);
+        next();
     },700)
       
 
-      app.get ('/info4', (req, res) =>{
+      app.get ('/info4', (req, res, next) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
   
         const intervalId = setInterval(() =>{
         const sendData4 = `data: ${tasaeuro}\n\n`;
         res.write(sendData4);
+        next();
       },700)
 
 
-      app.get ('/info5', (req, res) =>{
+      app.get ('/info5', (req, res, next) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
   
         const intervalId = setInterval(() =>{
           const sendData5 = `data: ${JSON.stringify(tasabinance) +' Bs.'}\n\n`;
           res.write(sendData5);
-
+          next();
       },700)
 
 
-      app.get ('/info6', (req, res) =>{
+      app.get ('/info6', (req, res, next) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
   
         const intervalId = setInterval(() =>{
           const sendData6 = `data: ${JSON.stringify(tasapaypal) +' Bs.'}\n\n`;
           res.write(sendData6);
-
+          next();
       },700)
 
-      app.get ('/info7', (req, res) =>{
+      app.get ('/info7', (req, res, next) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
   
         const intervalId = setInterval(() =>{
           const sendData7 = `data: ${JSON.stringify(cur_version)}\n\n`;
           res.write(sendData7);
-
+          next();
       },700)
 
      
