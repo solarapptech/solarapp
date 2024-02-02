531f4f11e04f1733fbbdadaac72190a8c3fa8b72
solarapptech <149142801+solarapptech@users.noreply.github.com>
2024-02-02 19:35:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,6 @@ let xval = 5;
 app.get ('/info1', (req, res) =>{
    res.setHeader('Content-Type', 'text/event-stream')
    res.setHeader('Access-Control-Allow-Origin', '*')
-   origin: '*';
 
     const intervalId = setInterval(() =>{
     const sendData = `data: ${JSON.stringify(tasabcv) +' Bs.'}\n\n`;
