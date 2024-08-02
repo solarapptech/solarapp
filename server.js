55d143d5bc6ef1c36f54db67731133d20b0d1144
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2024-08-02 09:10:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.headersTimeout = 81000;
 app.use(cors())
 
 // Fecha de Actualizado
-let fechas = "Jue, 1 - 1:00 PM";
+let fechas = "Vie, 2 - 9:05 AM";
 
 // Banco Central
 let tasabcv = 36.60;
