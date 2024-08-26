71d7e92a5b4f3c0a5d74c0777e59308e5e1a529d
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2024-08-26 08:55:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.headersTimeout = 81000;
 app.use(cors())
 
 // Fecha de Actualizado
-let fechas = "Lun, 26 - 7:00 AM";
+let fechas = "Lun, 26 - 9:00 AM";
 
 // Banco Central
 let tasabcv = 36.58;
@@ -17,10 +17,10 @@ let tasabcv = 36.58;
 let tasaeuro = 40.94;
 
 // Monitor Dolar
-let tasamonitor = 43.42;
+let tasamonitor = 42.60;
 
 // Binance
-let tasabinance = 43.39;
+let tasabinance = 42.99;
 
 // PayPal
 let tasapaypal = 38.30;
