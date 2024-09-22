7f1381af37b5509e34669a8e8894507a5649db84
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2024-09-21 22:00:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -59,11 +59,9 @@ app.get ('/info1', (req, res) =>{
    res.setHeader('Content-Type', 'text/event-stream')
    res.setHeader('Access-Control-Allow-Origin', '*')
 
-    const intervalId = setInterval(() =>{
     const sendData = `data: ${JSON.stringify(tasabcv) +' Bs.'}\n\n`;
     res.write(sendData);
 
-   },700)
 })
 
 // app.get ('/info1', (req, res) =>{
@@ -84,11 +82,10 @@ app.get ('/info2', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
    
-      const intervalId = setInterval(() =>{
+
       const sendData2 = `data: ${JSON.stringify(tasamonitor) +' Bs.'}\n\n`;
       res.write(sendData2);
 
-    },700)
 })
 
 
@@ -96,50 +93,49 @@ app.get ('/info2', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
        
-      const intervalId = setInterval(() =>{
         const sendData3 = `data: ${JSON.stringify(fechas)}\n\n`
         res.write(sendData3);
-    },700)
+       
   }) 
 
       app.get ('/info4', (req, res) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
          
-        const intervalId = setInterval(() =>{
         const sendData4 = `data: ${tasaeuro}\n\n`;
         res.write(sendData4);
-      },700)
+
+         
     })
 
       app.get ('/info5', (req, res) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
          
-        const intervalId = setInterval(() =>{
+
           const sendData5 = `data: ${JSON.stringify(tasabinance) +' Bs.'}\n\n`;
           res.write(sendData5);
-      },700)
+
     })
 
       app.get ('/info6', (req, res) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
          
-        const intervalId = setInterval(() =>{
+
           const sendData6 = `data: ${JSON.stringify(tasapaypal) +' Bs.'}\n\n`;
           res.write(sendData6);
-      },700)
+
     })
 
       app.get ('/info7', (req, res) =>{
         res.setHeader('Content-Type', 'text/event-stream')
         res.setHeader('Access-Control-Allow-Origin', '*')
          
-        const intervalId = setInterval(() =>{
+
           const sendData7 = `data: ${JSON.stringify(cur_version)}\n\n`;
           res.write(sendData7);
-      },700)
+
 })
      
     // app.get ('/info6', (req, res) =>{
@@ -182,10 +178,10 @@ res.setHeader("Access-Control-Allow-Origin", "*")
         "content-Type": "text/event-stream"
     });
 
-const intervalId = setInterval(() =>{
+
 const sendDatab = `data: ${JSON.stringify(tasabcv)}\n\n`;
 res.write(sendDatab);
-},700)
+
 })
 
 
