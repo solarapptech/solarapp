ea479473262c4f671eaeb3dc7152910f57647097
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2024-12-06 14:40:29 -0400

    Add files via upload

---
+++ eurapi.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+# bcv 
+url = 'https://www.bcv.org.ve/'
+response = requests.get(url, verify=False)
+
+if response.status_code == 200:
+    # Parseo de HTML
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Clase específica
+    elementos = soup.find_all(class_='col-sm-6 col-xs-6 centrado')
+
+    # Iterar el elemento y extraer el texto
+    for elemento in elementos[0]:
+        print(elemento.get_text().replace(',', '.'))
