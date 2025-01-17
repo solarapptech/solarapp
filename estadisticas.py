c14379527b42eb8d7b3e283f7c053eaeb7abcf89
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-01-17 16:29:52 -0400

    Update estadisticas.py

--- estadisticas.py
+++ estadisticas.py
@@ -1,24 +1,24 @@
 import os
-import requests
+# import requests
 from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 
 
 # bcv 
-url = 'https://www.bcv.org.ve/'
-response = requests.get(url, verify=False)
+# url = 'https://www.bcv.org.ve/'
+# response = requests.get(url, verify=False)
 
-if response.status_code == 200:
-    # Parseo de HTML
-    soup = BeautifulSoup(response.content, 'html.parser')
+# if response.status_code == 200:
+#     # Parseo de HTML
+#     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Clase específica
-    elementos = soup.find_all(class_='col-sm-6 col-xs-6 centrado')
+#     # Clase específica
+#     elementos = soup.find_all(class_='col-sm-6 col-xs-6 centrado')
 
-    # Iterar el elemento y extraer el texto
-    for elemento in elementos[4]:
-        valorbcv = elemento.get_text().replace(',', '.')
+#     # Iterar el elemento y extraer el texto
+#     for elemento in elementos[4]:
+#         valorbcv = elemento.get_text().replace(',', '.')
         # print(elemento.get_text().replace(',', '.'))
 
 
@@ -37,7 +37,7 @@ today = datetime.now()
 days_passed = (today - start_date).days
 
 # Generar tantos números aleatorios como días pasados más uno
-bcv_numbers = [str(valorbcv) for _ in range(days_passed + 1)]
+bcv_numbers = [str(5) for _ in range(days_passed + 1)]
 
 # Unir los números aleatorios y el contenido anterior si existe
 new_content = ", ".join(bcv_numbers)
