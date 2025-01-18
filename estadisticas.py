946054a30ab4d3cc2375da773953c5f565ef0a39
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-01-17 23:55:09 -0400

    Update estadisticas.py

--- estadisticas.py
+++ estadisticas.py
@@ -1,6 +1,3 @@
-import os
-from datetime import datetime, timedelta
-import pytz
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,36 +14,6 @@ if response.status_code == 200:
 
     # Iterar el elemento y extraer el texto
     for elemento in elementos[4]:
-        valor = elemento.get_text().replace(',', '.')
-        # print(elemento.get_text().replace(',', '.'))
-
-    # Definir la zona horaria de Venezuela
-    venezuela_tz = pytz.timezone('America/Caracas')
-
-    # Leer el contenido actual del archivo
-    if os.path.exists('output.txt'):
-        with open('output.txt', 'r') as file:
-            content = file.read().strip()
-    else:
-        content = ""
-
-    # Convertir el contenido en una lista
-    bcv_numbers = content.split(", ") if content else []
-
-    # Contar el número de días desde una fecha de inicio en la zona horaria de Venezuela
-    start_date = datetime(2025, 1, 16, tzinfo=venezuela_tz)
-    today = datetime.now(venezuela_tz)
-    days_passed = (today - start_date).days
-
-    # Generar un número aleatorio solo para el día actual
-    if len(bcv_numbers) <= days_passed:
-        bcv_numbers.append(elemento.get_text().replace(',', '.'))  # Puedes ajustar el rango según tus necesidades
-    else:
-        bcv_numbers[days_passed] = str(elemento.get_text().replace(',', '.'))
-
-    # Unir los números y el contenido anterior si existe
-    new_content = ", ".join(bcv_numbers)
-
-    # Escribir el nuevo contenido en el archivo (sin saltos de línea)
-    with open('output.txt', 'w') as file:
-        file.write(new_content)
+        with open("output.txt", "w") as file:
+            file.write(elemento.get_text().replace(',', '.'))
+            # print(elemento.get_text().replace(',', '.'))
