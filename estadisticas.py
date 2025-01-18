8e00b8213947cd4401cf0ea455729a3381f18f06
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-01-17 21:51:46 -0400

    Update estadisticas.py

--- estadisticas.py
+++ estadisticas.py
@@ -17,7 +17,7 @@ else:
 bcv_numbers = content.split(", ") if content else []
 
 # Contar el número de días desde una fecha de inicio en la zona horaria de Venezuela
-start_date = datetime(2025, 1, 17, tzinfo=venezuela_tz)
+start_date = datetime(2025, 1, 16, tzinfo=venezuela_tz)
 today = datetime.now(venezuela_tz)
 days_passed = (today - start_date).days
 
