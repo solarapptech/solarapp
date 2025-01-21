84460b28c1b6648adacb13025841ed25f0c0bc3c
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-01-21 11:08:37 -0400

    Update estadisticas.py

--- estadisticas.py
+++ estadisticas.py
@@ -48,5 +48,5 @@ if response.status_code == 200:
         file.write('')
     # Escribir el nuevo contenido en el archivo (sin saltos de línea)
     with open('output.js', 'w') as file:
-        file.write(f"const graf_bcv = [{new_content}];\nmodule.exports = {{ graf_bcv }};")
-        # file.write(f"const graf_bcv = [{new_content}];\n")
+        file.write(f"const graf_bcv = [{new_content}];\n")
+        file.write("module.exports = {{ graf_bcv }};")
