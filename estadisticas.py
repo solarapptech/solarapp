bf171c2540b535708da5a9419b52cbda87e9d6fd
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-01-21 21:07:01 -0400

    Update estadisticas.py

--- estadisticas.py
+++ estadisticas.py
@@ -11,10 +11,8 @@ response = requests.get(url, verify=False)
 if response.status_code == 200:
     # Parseo de HTML
     soup = BeautifulSoup(response.content, 'html.parser')
-
     # Clase específica
     elementos = soup.find_all(class_='col-sm-6 col-xs-6 centrado')
-
     valor = elementos[4].get_text().replace(',', '.') if len(elementos) > 4 else "N/A"
 
     # Definir la zona horaria de Venezuela
@@ -28,9 +26,7 @@ if response.status_code == 200:
         content = ""
 
     # Convertir el contenido en una lista
-    # bcv_numbers = content.split(", ") if content else []
     bcv_numbers = content.replace("const graf_bcv = [", "").replace("];\nmodule.exports = { graf_bcv };", "").split(", ") if content else []
-    # bcv_numbers = content.replace("const graf_bcv = [", "").replace("];\nmodule.exports = { graf_bcv };", "").split(", ") if content else []
     
     # Contar el número de días desde una fecha de inicio en la zona horaria de Venezuela
     start_date = datetime(2025, 1, 20, tzinfo=venezuela_tz)
@@ -39,17 +35,13 @@ if response.status_code == 200:
 
     # Generar un número aleatorio solo para el día actual
     if len(bcv_numbers) <= days_passed:
-        bcv_numbers.append(str(valor))  # Puedes ajustar el rango según tus necesidades
+        bcv_numbers.append(str(valor)) 
     else:
         bcv_numbers[days_passed] = str(valor)
 
     # Unir los números y el contenido anterior si existe
     new_content = ", ".join(bcv_numbers)
     
-   # with open('output.js', 'w') as file:
-       # file.write('')
     # Escribir el nuevo contenido en el archivo (sin saltos de línea)
     with open('output.js', 'w') as file:
-        # file.write(f"const graf_bcv = [{new_content}];\n")
-        # file.write("module.exports = {{ graf_bcv }};")
-          file.write(f"const graf_bcv = [{new_content}];\nmodule.exports = {{ graf_bcv }};")
+        file.write(f"const graf_bcv = [{new_content}];\nmodule.exports = {{ graf_bcv }};")
