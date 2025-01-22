4bf969d2fbef9bc03552c45228e4c0861748237a
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-01-22 01:31:25 -0400

    Update estadisticas_paralelo.py

--- output/estadisticas_paralelo.py
+++ output/estadisticas_paralelo.py
@@ -1,4 +1,5 @@
-import execjs
+import requests
+import re
 import os
 from datetime import datetime, timedelta
 import pytz
@@ -8,40 +9,44 @@ directorio = 'output'
 archivo = 'outputParalelo.js'
 ruta = os.path.join(directorio, archivo)
 
+# URL 
+url = 'https://github.com/solarapptech/solarapp/blob/main/server.js'
 
-with open('server.js', 'r') as file:
-    js_code = file.read()
+# Contenido del archivo
+response = requests.get(url)
+content = response.text
+# Encontrar el valor de paral
+match = re.search(r'const paral = (\d+);', content)
 
-ctx = execjs.compile(js_code)
-paral = ctx.eval('paral')
+if response.status_code == 200:
 
-# Zona horaria de Venezuela
-venezuela_tz = pytz.timezone('America/Caracas')
+    # Definir la zona horaria de Venezuela
+    venezuela_tz = pytz.timezone('America/Caracas')
 
-# Leer el contenido actual del archivo
-if os.path.exists(ruta):
-    with open(ruta, 'r') as file:
-        content = file.read().strip()
-else:
-    content = ""
+    # Leer el contenido actual del archivo
+    if os.path.exists(ruta):
+        with open(ruta, 'r') as file:
+            content = file.read().strip()
+    else:
+        content = ""
 
-# Convertir el contenido en una lista
-paralelo_numbers = content.replace("const graf_paralelo = [", "").replace("];\nmodule.exports = { graf_paralelo };", "").split(", ") if content else []
+    # Convertir el contenido en una lista
+    paralelo_numbers = content.replace("const graf_paralelo = [", "").replace("];\nmodule.exports = { graf_paralelo };", "").split(", ") if content else []
+    
+    # Contar el número de días desde una fecha de inicio en la zona horaria de Venezuela
+    start_date = datetime(2025, 1, 20, tzinfo=venezuela_tz)
+    today = datetime.now(venezuela_tz)
+    days_passed = (today - start_date).days
 
-# Contar el número de días desde una fecha de inicio en la zona horaria de Venezuela
-start_date = datetime(2025, 1, 20, tzinfo=venezuela_tz)
-today = datetime.now(venezuela_tz)
-days_passed = (today - start_date).days
+    # Check de tiempo
+    if len(paralelo_numbers) <= days_passed:
+        paralelo_numbers.append(str(match)) 
+    else:
+        paralelo_numbers[days_passed] = str(match)
 
-# Check de tiempo
-if len(paralelo_numbers) <= days_passed:
-    paralelo_numbers.append(str(paral)) 
-else:
-    paralelo_numbers[days_passed] = str(paral)
-
-# Unir los números y el contenido anterior si existe
-new_content = ", ".join(paralelo_numbers)
-
-# Escribir el nuevo contenido en el archivo (sin saltos de línea)
-with open(ruta, 'w') as file:
-    file.write(f"const graf_paralelo = [{new_content}];\nmodule.exports = {{ graf_paralelo}};")
+    # Unir los números y el contenido anterior si existe
+    new_content = ", ".join(paralelo_numbers)
+    
+    # Escribir el nuevo contenido en el archivo (sin saltos de línea)
+    with open(ruta, 'w') as file:
+        file.write(f"const graf_paralelo = [{new_content}];\nmodule.exports = {{ graf_paralelo }};")
