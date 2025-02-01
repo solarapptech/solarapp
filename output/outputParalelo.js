2696a01433cc1df4ced8369ea449698774d83d31
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-02-01 14:33:03 -0400

    Update outputParalelo.js

--- output/outputParalelo.js
+++ output/outputParalelo.js
@@ -1,2 +1,2 @@
-const graf_paralelo = [57.44, 57.44, 58.61, 58.77, 57.67, 57.67, 57.67, 58.29, 60.03, 61.23, 61.0, 59.95, 68.34];
+const graf_paralelo = [57.44,];
 module.exports = { graf_paralelo };
