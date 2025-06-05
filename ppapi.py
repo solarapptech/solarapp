3c81409bc82864b1364118a4ffcc49e57c6dddd0
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2025-06-04 22:17:03 -0400

    Update ppapi.py

--- ppapi.py
+++ ppapi.py
@@ -1,11 +1,22 @@
 import requests
-import json
+from bs4 import BeautifulSoup
 
-url = 'https://pydolarve.org/api/v2/dollar?page=criptodolar'
+url = "https://www.intergiros.com/"
+
 response = requests.get(url)
+html_content = response.text
 
-data = json.loads(response.text)
+soup = BeautifulSoup(html_content, "html.parser")
 
-price = data["monitors"]["paypal"]["price"]
+info_box = soup.find("div", class_="aio-icon-description ult-responsive")
 
-print(price * 1)
+if info_box:
+    p_element = info_box.find("p")
+    if p_element and p_element.find("em"):
+        texto = p_element.text 
+        resultado = texto.split("=")[1].strip()
+        print(resultado)
+    else:
+        print("0 Bs.")
+else:
+    print("0 Bs.")
